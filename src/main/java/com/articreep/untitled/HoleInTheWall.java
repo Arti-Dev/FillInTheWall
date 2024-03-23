6787e72cd6fbe720f3729f7641d0c32396358754
Arti-Dev <boomcreeper296@gmail.com>
2024-03-23 12:03:52 -0400

    Initial commit

---
+++ src/main/java/com/articreep/untitled/HoleInTheWall.java
@@ -0,0 +1,17 @@
+package com.articreep.untitled;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HoleInTheWall extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
