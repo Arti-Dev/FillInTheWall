eedd9185b0b2097f00fd6369e84779be7871b8dc
Arti-Dev <boomcreeper296@gmail.com>
2024-05-15 14:36:03 -0400

    Add barrier puddle visual effect

--- src/main/java/com/articreep/holeinthewall/HoleInTheWall.java
+++ src/main/java/com/articreep/holeinthewall/HoleInTheWall.java
@@ -1,5 +1,6 @@
 package com.articreep.holeinthewall;
 
+import com.articreep.holeinthewall.environments.TheVoid;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,6 +13,7 @@ public final class HoleInTheWall extends JavaPlugin {
         instance = this;
         getCommand("holeinthewall").setExecutor(this);
         getServer().getPluginManager().registerEvents(new PlayingFieldManager(), this);
+        getServer().getPluginManager().registerEvents(new TheVoid(), this);
         Bukkit.getLogger().info(ChatColor.BLUE + "HoleInTheWall has been enabled!");
         System.out.println("lmao");
 
---
+++ src/main/java/com/articreep/holeinthewall/environments/TheVoid.java
@@ -0,0 +1,62 @@
+package com.articreep.holeinthewall.environments;
+
+import com.articreep.holeinthewall.HoleInTheWall;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TheVoid implements Listener {
+
+    private static Set<Player> puddleCooldowns = new HashSet<>();
+    @EventHandler
+    public static void puddleParticles(PlayerMoveEvent event) {
+        if (puddleCooldowns.contains(event.getPlayer())) {
+            return;
+        }
+        Player player = event.getPlayer();
+        Location location = player.getLocation();
+        if (location.clone().add(0, -0.05, 0).getBlock().getType() != Material.BARRIER) return;
+
+        // x = 0.5t-0.5sin(t/2) y = 0.25 - 0.25cos(t)
+        // fan out in 360 degrees, 10 degree increments
+
+        puddleCooldowns.add(player);
+        Bukkit.getScheduler().runTaskLater(HoleInTheWall.getInstance(),
+                () -> puddleCooldowns.remove(player), 20 * 5);
+
+        Particle particle = Particle.DRIPPING_OBSIDIAN_TEAR;
+
+        new BukkitRunnable() {
+            double t = 0;
+            double tMax = Math.PI * 2;
+            double tIncrement = Math.PI / 4;
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 360; i += 10) {
+                    Vector vector = new Vector(1, 0, 0);
+                    vector.rotateAroundY(i);
+                    double x = 0.125 * t - 0.125 * Math.sin(t / 2);
+                    double y = 0.125 - 0.125 * Math.cos(t);
+                    // spawn particle at x, y
+                    player.spawnParticle(particle, location.clone().add(vector.clone().multiply(x)).add(0, y, 0), 1);
+                }
+
+                t += tIncrement;
+                if (t >= tMax) {
+                    cancel();
+                }
+            }
+        }.runTaskTimer(HoleInTheWall.getInstance(), 0, 1);
+    }
+}
