ed3735a528610d881b57963d603630bac7e83e9e
Arti-Dev <boomcreeper296@gmail.com>
2025-03-11 19:22:36 -0400

    Implement activation sound for scale

--- src/main/java/com/articreep/fillinthewall/modifiers/Scale.java
+++ src/main/java/com/articreep/fillinthewall/modifiers/Scale.java
@@ -42,14 +42,6 @@ public class Scale extends ModifierEvent implements Listener {
         } else {
             color = ChatColor.BLUE;
         }
-        final double DEFAULT_BLOCK_INTERACTION_RANGE = 4.5;
-        double blockInteractionRange = DEFAULT_BLOCK_INTERACTION_RANGE * Math.max(scale, 1);
-        for (Player player : field.getPlayers()) {
-            playerScales.put(player, player.getAttribute(Attribute.SCALE).getBaseValue());
-            player.getAttribute(Attribute.SCALE).setBaseValue(scale);
-            playerReachDistances.put(player, player.getAttribute(Attribute.BLOCK_INTERACTION_RANGE).getBaseValue());
-            player.getAttribute(Attribute.BLOCK_INTERACTION_RANGE).setBaseValue(blockInteractionRange);
-        }
 
         new BukkitRunnable() {
             int i = 0;
@@ -59,24 +51,22 @@ public class Scale extends ModifierEvent implements Listener {
                     cancel();
                     return;
                 }
-
                 double diff = scale - 1;
                 double toDisplay = 1 + (diff * i/20.0);
-
-                float pitch;
-                if (scale <= 1) {
-                    pitch = (float) Math.pow(2, -(1-toDisplay));
-
-                } else {
-                    pitch = (float) Math.pow(2, toDisplay/5);
-                }
-                field.playSoundToPlayers(Sound.BLOCK_NOTE_BLOCK_HARP, 5, pitch);
 
                 field.sendTitleToPlayers("Scale!", "Your player model has been scaled by " +
                         color + String.format("%.2f", toDisplay) + ChatColor.RESET + "!", 0, 40, 10);
                 i++;
             }
         }.runTaskTimer(FillInTheWall.getInstance(), 0, 1);
+        final double DEFAULT_BLOCK_INTERACTION_RANGE = 4.5;
+        double blockInteractionRange = DEFAULT_BLOCK_INTERACTION_RANGE * Math.max(scale, 1);
+        for (Player player : field.getPlayers()) {
+            playerScales.put(player, player.getAttribute(Attribute.SCALE).getBaseValue());
+            player.getAttribute(Attribute.SCALE).setBaseValue(scale);
+            playerReachDistances.put(player, player.getAttribute(Attribute.BLOCK_INTERACTION_RANGE).getBaseValue());
+            player.getAttribute(Attribute.BLOCK_INTERACTION_RANGE).setBaseValue(blockInteractionRange);
+        }
     }
 
     @EventHandler
@@ -104,18 +94,6 @@ public class Scale extends ModifierEvent implements Listener {
         for (Player player : field.getPlayers()) {
             resetPlayer(player);
         }
-        new BukkitRunnable() {
-            int i = 0;
-            @Override
-            public void run() {
-                if (i >= 3) {
-                    cancel();
-                    return;
-                }
-                field.playSoundToPlayers(Sound.BLOCK_NOTE_BLOCK_HARP, 5, 1);
-                i++;
-            }
-        }.runTaskTimer(FillInTheWall.getInstance(), 0, 1);
         field.sendTitleToPlayers("", "Your player model has been reset!", 0, 20, 10);
     }
 
@@ -128,11 +106,44 @@ public class Scale extends ModifierEvent implements Listener {
 
     @Override
     public void playActivateSound() {
+        new BukkitRunnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                if (i > 20) {
+                    cancel();
+                    return;
+                }
 
+                double diff = scale - 1;
+                double toDisplay = 1 + (diff * i/20.0);
+
+                float pitch;
+                if (scale <= 1) {
+                    pitch = (float) Math.pow(2, -(1-toDisplay));
+
+                } else {
+                    pitch = (float) Math.pow(2, toDisplay/5);
+                }
+                field.playSoundToPlayers(Sound.BLOCK_NOTE_BLOCK_HARP, 5, pitch);
+                i++;
+            }
+        }.runTaskTimer(FillInTheWall.getInstance(), 0, 1);
     }
 
     @Override
     public void playDeactivateSound() {
-
+        new BukkitRunnable() {
+            int i = 0;
+            @Override
+            public void run() {
+                if (i >= 3) {
+                    cancel();
+                    return;
+                }
+                field.playSoundToPlayers(Sound.BLOCK_NOTE_BLOCK_HARP, 5, 1);
+                i++;
+            }
+        }.runTaskTimer(FillInTheWall.getInstance(), 0, 2);
     }
 }
