5039f2236693f5e4dc1da4a5ff5ba7c82a60c4ec
Aidan Liu <boomcreeper296@gmail.com>
2025-07-02 22:15:35 -0700

    Add database check for chat messages

--- src/main/java/com/articreep/fillinthewall/GlobalListeners.java
+++ src/main/java/com/articreep/fillinthewall/GlobalListeners.java
@@ -119,6 +119,7 @@ public class GlobalListeners implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onChat(AsyncChatEvent event) {
         if (event.isCancelled()) return;
+        if (Database.isOfflineMode()) return;
         event.setCancelled(true);
         Player player = event.getPlayer();
         Component prefix;
