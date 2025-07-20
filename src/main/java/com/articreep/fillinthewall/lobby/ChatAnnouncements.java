ca164c374ec777250314185d196f7196ba7c8a4e
Aidan Liu <boomcreeper296@gmail.com>
2025-07-19 20:45:19 -0400

    One more tip

--- src/main/java/com/articreep/fillinthewall/lobby/ChatAnnouncements.java
+++ src/main/java/com/articreep/fillinthewall/lobby/ChatAnnouncements.java
@@ -20,7 +20,8 @@ public class ChatAnnouncements {
             miniMessage.deserialize("<blue>[⿻] <yellow>Pair up with a friend in Multiplayer with /fitw pair!"),
             miniMessage.deserialize("<blue>[⿻] <yellow>To leave a game, simply step off the playing field."),
             miniMessage.deserialize("<blue>[⿻] <yellow>Don't want to play Multiplayer? Run /fitw spectate."),
-            miniMessage.deserialize("<blue>[⿻] <yellow>You can toggle <red>Gimmickless</red> mode with the Blaze Powder in your inventory (when not in a game).")
+            miniMessage.deserialize("<blue>[⿻] <yellow>You can toggle <red>Gimmickless</red> mode with the Blaze Powder in your inventory (when not in a game)."),
+            miniMessage.deserialize("<blue>[⿻] <yellow>You can disable these tips by clicking your head in your ninth slot (when not in a game).")
     };
     private static BukkitTask announcementTask;
 
