63843587b01ee7bda7ac7bbb54a9bed327d13a28
Aidan Liu <boomcreeper296@gmail.com>
2025-10-25 22:39:06 -0400

    Pregame timer worlds are now configurable

--- src/main/java/com/articreep/fillinthewall/FillInTheWall.java
+++ src/main/java/com/articreep/fillinthewall/FillInTheWall.java
@@ -101,11 +101,13 @@ public final class FillInTheWall extends JavaPlugin implements Listener {
                 getSLF4JLogger().info("NoteBlockAPI not found - note block music disabled");
                 NBSMusic.enabled = false;
             }
-
-            PlayingFieldManager.pregame = new Pregame(Bukkit.getWorld("multi"), Gamemode.MULTIPLAYER_SCORE_ATTACK,
-                    2, 60);
+            String multiplayerWorldName = getConfig().getString("pregame-worlds.multiplayer", "multi");
+            String versusWorldName = getConfig().getString("pregame-worlds.versus", "versus");
+            int countdownMax = getConfig().getInt("pregame-countdown", 60);
+            PlayingFieldManager.pregame = new Pregame(Bukkit.getWorld(multiplayerWorldName), Gamemode.MULTIPLAYER_SCORE_ATTACK,
+                    2, countdownMax);
             PlayingFieldManager.pregame.startCountdown();
-            PlayingFieldManager.vsPregame = new Pregame(Bukkit.getWorld("versus"), Gamemode.VERSUS, 2, 15);
+            PlayingFieldManager.vsPregame = new Pregame(Bukkit.getWorld(versusWorldName), Gamemode.VERSUS, 2, countdownMax);
             PlayingFieldManager.parseConfig(getPlayingFieldConfig());
             spawnPortals();
             Leaderboards.spawnLeaderboards(getConfig());
